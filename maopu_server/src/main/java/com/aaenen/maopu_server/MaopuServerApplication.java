fffa52524775512a00f54683c4c2f674b8f658c1
aaenen <1521042939@qq.com>
2021-03-12 14:52:36 +0800

    no message

---
+++ maopu_server/src/main/java/com/aaenen/maopu_server/MaopuServerApplication.java
@@ -0,0 +1,18 @@
+package com.aaenen.maopu_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 热更新  热加载
+ * 1.cmd + shift + A ————>搜索registry，找到Registry...，注意是带三个点的，然后找到complier.automake.allow.when.app.running,勾选
+ * 2.改完代码时候，cmd + F9可以进行热加载
+ */
+@SpringBootApplication
+public class MaopuServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaopuServerApplication.class, args);
+    }
+
+}
---
+++ maopu_server/src/test/java/com/aaenen/maopu_server/MaopuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaenen.maopu_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaopuServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
