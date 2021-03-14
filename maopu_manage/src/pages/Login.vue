d0f4870bd56055f7d3c184fafe7de47b4ef15c36
aaenen <1521042939@qq.com>
2021-03-14 13:07:05 +0800

    补充了创建时没有的东西

---
+++ maopu_manage/src/pages/Login.vue
@@ -0,0 +1,36 @@
+<!--
+ * @Author: aaenen
+ * @Date: 2021-03-13 22:16:50
+ * @LastEditTime: 2021-03-13 22:26:08
+ * @LastEditors: aaenen
+ * @Description:猫扑 后台登录管理
+ * @FilePath: /maopu_manage/src/pages/Login.vue
+ * 打码不易，且行且珍惜！
+-->
+<template>
+  <div class="login-wrap">
+    <div class="ms-title">猫扑 后台登录管理</div>
+  </div>
+</template>
+
+<style scoped>
+.login-wrap{
+  position: relative;
+  background: url("../assets/img/background.jpg");
+  background-attachment: fixed;
+  background-position: center;
+  border-style: cover;
+  width: 100%;
+  height: 100%;
+}
+.ms-title{
+  position: absolute;
+  top: 50%;
+  width: 100%;
+  margin-top: -230px;
+  text-align: center;
+  font-size: 30px;
+  font-weight: 600;
+  color: #fff;
+}
+</style>
